108fdd214e34986c76a2466a3abbc16b4cf05d05
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2024-04-10 19:46:44 +0530

    Update openmp.cpp

--- openmp.cpp
+++ openmp.cpp
@@ -3,11 +3,13 @@
 #include <ctime>
 #include <chrono>
 #include <fstream>
-#include <omp.h> // Include OpenMP library
+#include <omp.h>
 
 using namespace std::chrono;
 
-const int MATRIX_DIM = 200;
+const int MATRIX_DIM = 300;
+const int NUM_PROCESSES = 4;
+const int NUM_THREADS = 2;
 
 void fillMatrixWithRandomValues(int matrix[][MATRIX_DIM]) {
     for (int row = 0; row < MATRIX_DIM; ++row)
@@ -16,7 +18,7 @@ void fillMatrixWithRandomValues(int matrix[][MATRIX_DIM]) {
 }
 
 void calculateProductOfMatrices(const int firstMatrix[][MATRIX_DIM], const int secondMatrix[][MATRIX_DIM], int productMatrix[][MATRIX_DIM], int startRow, int endRow) {
-    #pragma omp parallel for
+    #pragma omp parallel for num_threads(NUM_THREADS)
     for (int row = startRow; row < endRow; ++row) {
         for (int col = 0; col < MATRIX_DIM; ++col) {
             productMatrix[row][col] = 0;
@@ -28,8 +30,7 @@ void calculateProductOfMatrices(const int firstMatrix[][MATRIX_DIM], const int s
 }
 
 int main() {
-    srand(time(nullptr)); // Seed the random number generator
-
+    srand(time(nullptr));
     int firstMatrix[MATRIX_DIM][MATRIX_DIM];
     int secondMatrix[MATRIX_DIM][MATRIX_DIM];
     int productMatrix[MATRIX_DIM][MATRIX_DIM] = {0};
@@ -40,23 +41,22 @@ int main() {
     auto startMoment = high_resolution_clock::now();
 
     // Calculate product of matrices using OpenMP
-    calculateProductOfMatrices(firstMatrix, secondMatrix, productMatrix, 0, MATRIX_DIM);
+    int rowsPerProcess = MATRIX_DIM / NUM_PROCESSES;
+    #pragma omp parallel for num_threads(NUM_PROCESSES)
+    for (int i = 0; i < NUM_PROCESSES; ++i) {
+        int startRow = i * rowsPerProcess;
+        int endRow = (i + 1) * rowsPerProcess;
+        calculateProductOfMatrices(firstMatrix, secondMatrix, productMatrix, startRow, endRow);
+    }
 
     auto endMoment = high_resolution_clock::now();
-    auto timeTaken = duration_cast<microseconds>(endMoment - startMoment);
+    auto timeTaken = duration_cast<seconds>(endMoment - startMoment);
 
-    std::cout << "Multiplication completed in: " << timeTaken.count() << " microseconds" << std::endl;
-
-    // Write the result matrix and execution time to a file
-    std::ofstream resultFile("Result_matrix.txt");
-    for (int row = 0; row < MATRIX_DIM; ++row) {
-        for (int col = 0; col < MATRIX_DIM; ++col) {
-            resultFile << productMatrix[row][col] << "\t";
-        }
-        resultFile << std::endl;
-    }
-    resultFile << "Execution time: " << timeTaken.count() << " microseconds";
-    resultFile.close();
+    std::cout << "MPI Matrix Multiplication Performance with OpenMP" << std::endl;
+    std::cout << "Dimension: " << MATRIX_DIM << std::endl;
+    std::cout << "Processes: " << NUM_PROCESSES << std::endl;
+    std::cout << "Threads: " << NUM_THREADS << std::endl;
+    std::cout << "Runtime: " << timeTaken << std::endl;
 
     return 0;
 }
